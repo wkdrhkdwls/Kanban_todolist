7339c4bae9dc4a0c11cf70920f88cf5c2688d013
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2022-11-16 18:04:53 +0900

    localStorage
    localStorage

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,5 +1,7 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
+const { persistAtom } = recoilPersist();
 export interface ITodo {
   id: number;
   text: string;
@@ -16,4 +18,11 @@ export const toDoState = atom<IToDoState>({
     Doing: [],
     Done: [],
   },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const BoardState = atom<string[]>({
+  key: "boards",
+  default: ["To Do", "Doing", "Done"],
+  effects_UNSTABLE: [persistAtom],
 });
